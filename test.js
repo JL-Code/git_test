1dc6c95ce102595833af9dbd083750b877a58d84
jiangy <jiangliu0316@outlook.com>
2019-05-27 00:11:48 +0800

    添加test.js

---
+++ test.js
@@ -0,0 +1,8 @@
+var test = localStorage.getItem("test");
+
+console.log("test", test);
+
+localStorage.setItem("test", 1);
+console.log("setItem test after", test);
+
+localStorage.setItem("test2", "跨域写入操作");
