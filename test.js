d64f64bff0e5ea852a8230bb4ae9dd25935f4941
jiangy <jiangliu0316@outlook.com>
2019-05-27 00:16:12 +0800

    修改读取操作

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
-var test = localStorage.getItem("test");
+// var test = localStorage.getItem("test");
 
-console.log("test", test);
+// console.log("test", test);
 
 localStorage.setItem("test", 1);
-console.log("setItem test after", test);
+// console.log("setItem test after", test);
 
 localStorage.setItem("test2", "跨域写入操作");
