5d39dc73858264c4897254b3792b50b26e72aeea
蒋勇 <jiangy@highzap.com>
2020-03-13 10:28:20 +0800

    更新gitflow.sh

--- gitflow.sh
+++ gitflow.sh
@@ -6,7 +6,8 @@ git branch
 git flow release start v1.0.0.test
 # 可能的修改版本号
 # 开始 release 分支
-git flow release finish v1.0.0.test
+git flow release finish v1.0.0.test -m 'release 消息'
 # 输入 release 信息
 
-# 推送分支
+# 推送本地所有分支
+git push origin --all
