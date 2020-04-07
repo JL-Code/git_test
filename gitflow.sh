fe72afb5601f4a2945e6475a00db17111ecb8905
蒋勇 <jiangy@highzap.com>
2020-04-07 14:56:05 +0800

    update gitflow.sh

--- gitflow.sh
+++ gitflow.sh
@@ -1,13 +1,13 @@
 # 拉取仓库代码,并初始 git flow
 git clone https://github.com/JL-Code/git_test.git
 git flow init -d
-git branch
+echo '当前分支信息👇'
+git branch -av
 # 创建release分支
 git flow release start v1.0.0.test
-# 可能的修改版本号
 # 开始 release 分支
-git flow release finish v1.0.0.test -m 'release 消息'
-# 输入 release 信息
-
+git flow release finish -m 'release 消息' v1.0.0.test
 # 推送本地所有分支
 git push origin --all
+echo '所有分支信息👇'
+git branch -av
